3cd037866bdf2634cb293991f896677048fd7e88
Yanpeng Lin <ypenglyn@gmail.com>
2018-02-04 10:36:18 +0900

    add base hash function

--- solr-lsh/src/main/java/info/ypenglyn/lsh/EuclideanHash.java
+++ solr-lsh/src/main/java/info/ypenglyn/lsh/EuclideanHash.java
@@ -1,7 +1,16 @@
 package info.ypenglyn.lsh;
 
 import java.util.BitSet;
+import java.util.Random;
+
+import org.apache.commons.math3.analysis.UnivariateFunction;
 import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.BlockRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.random.GaussianRandomGenerator;
+import org.apache.commons.math3.random.RandomGeneratorFactory;
+import org.apache.commons.math3.random.UncorrelatedRandomVectorGenerator;
 
 /**
  * A implementation of https://people.csail.mit.edu/indyk/p117-andoni.pdf.
@@ -9,7 +18,8 @@ import org.apache.commons.math3.linear.ArrayRealVector;
 public class EuclideanHash implements VHash {
 
     private static final long serialVersionUID = -7604766932017737126L;
-    private static final long primeP = 4294967291L;
+    private static final long primeNumber = 4294967291L;
+    private RealMatrix hashDirection;
 
     private EuclideanHash() {
     }
@@ -23,11 +33,48 @@ public class EuclideanHash implements VHash {
     }
 
     /**
-     * @param k, hyper parameter
+     * @param dim,  dimension of data vector
+     * @param k,    hyper parameter
+     * @param l,    hash batch size
+     * @param seed, random seed
      * @return EuclideanHash instance
      */
-    public EuclideanHash init(int k) {
+    public EuclideanHash init(int dim, int k, int l, long seed) {
+        this.hashDirection = generateRandomHashDirection(dim, seed);
         return this;
+    }
+
+    /**
+     * @param dim,  dimension of original data vector
+     * @param seed, random seed
+     * @return RealMatrix that contains directions for hash function
+     * TODO: this function is kind of same as {@link SuperBitHash#generateRandomHyperplane(int, int, long)}
+     */
+    protected RealMatrix generateRandomHashDirection(int dim, long seed) {
+        // 2 random directions for constructing base hash function
+        RealMatrix direction = new BlockRealMatrix(dim, 2);
+
+        GaussianRandomGenerator rawGenerator = new GaussianRandomGenerator(
+                RandomGeneratorFactory.createRandomGenerator(new Random(seed)));
+        UncorrelatedRandomVectorGenerator vectorGenerator = new UncorrelatedRandomVectorGenerator(
+                dim, rawGenerator);
+
+        for (int i = 0; i < 2; i++) {
+            direction.setColumn(i, vectorGenerator.nextVector());
+        }
+
+        return direction;
+    }
+
+    /**
+     * @param toHash,  vector to be hashed
+     * @param hashIdx, index of current hash function
+     * @return double, hashed value
+     */
+    protected double calculateHash(RealVector toHash, int hashIdx) {
+        RealVector raw = this.hashDirection.preMultiply(toHash);
+        raw.mapToSelf(new ModPrime());
+        return raw.getEntry(0) + hashIdx * raw.getEntry(1);
     }
 
     @Override
@@ -53,5 +100,12 @@ public class EuclideanHash implements VHash {
     private static class HasherHolder {
 
         private static final EuclideanHash instance = EuclideanHash.getNewInstance();
+    }
+
+    private static class ModPrime implements UnivariateFunction {
+        @Override
+        public double value(double x) {
+            return x % EuclideanHash.primeNumber;
+        }
     }
 }
