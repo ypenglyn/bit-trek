f4be78c135184ac30aa558c8133dd35fd9e0e838
Yanpeng Lin <ypenglyn@gmail.com>
2018-02-04 16:51:26 +0900

    under refactoring

--- solr-lsh/src/main/java/info/ypenglyn/lsh/EuclideanHash.java
+++ solr-lsh/src/main/java/info/ypenglyn/lsh/EuclideanHash.java
@@ -1,5 +1,8 @@
 package info.ypenglyn.lsh;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.DoubleBuffer;
 import java.util.BitSet;
 import java.util.Random;
 
@@ -19,7 +22,10 @@ public class EuclideanHash implements VHash {
 
     private static final long serialVersionUID = -7604766932017737126L;
     private static final long primeNumber = 4294967291L;
-    private RealMatrix hashDirection;
+    private int k;
+    private int l;
+    private RealMatrix hash1;
+    private RealMatrix hash2;
 
     private EuclideanHash() {
     }
@@ -40,51 +46,67 @@ public class EuclideanHash implements VHash {
      * @return EuclideanHash instance
      */
     public EuclideanHash init(int dim, int k, int l, long seed) {
-        this.hashDirection = generateRandomHashDirection(dim, seed);
+        this.k = k;
+        this.l = l;
+        this.hash1 = generateRandomHashDirection(dim, l, seed);
+        this.hash2 = generateRandomHashDirection(dim, l, seed + 1);
         return this;
     }
 
     /**
      * @param dim,  dimension of original data vector
+     * @param l,    hash batch size
      * @param seed, random seed
      * @return RealMatrix that contains directions for hash function
-     * TODO: this function is kind of same as {@link SuperBitHash#generateRandomHyperplane(int, int, long)}
+     * TODO: this function is duplicated to {@link SuperBitHash#generateRandomHyperplane(int, int, long)}
      */
-    protected RealMatrix generateRandomHashDirection(int dim, long seed) {
-        // 2 random directions for constructing base hash function
-        RealMatrix direction = new BlockRealMatrix(dim, 2);
+    protected RealMatrix generateRandomHashDirection(int dim, int l, long seed) {
+        RealMatrix direction = new BlockRealMatrix(dim, l);
 
         GaussianRandomGenerator rawGenerator = new GaussianRandomGenerator(
                 RandomGeneratorFactory.createRandomGenerator(new Random(seed)));
         UncorrelatedRandomVectorGenerator vectorGenerator = new UncorrelatedRandomVectorGenerator(
                 dim, rawGenerator);
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < l; i++) {
             direction.setColumn(i, vectorGenerator.nextVector());
         }
 
         return direction;
     }
 
-    /**
-     * @param toHash,  vector to be hashed
-     * @param hashIdx, index of current hash function
-     * @return double, hashed value
-     */
-    protected double calculateHash(RealVector toHash, int hashIdx) {
-        RealVector raw = this.hashDirection.preMultiply(toHash);
-        raw.mapToSelf(new ModPrime());
-        return raw.getEntry(0) + hashIdx * raw.getEntry(1);
-    }
-
     @Override
     public BitSet hash(double[] vector) {
-        return null;
+        double[] h1 = this.hash1.preMultiply(vector);
+        double[] h2 = this.hash2.preMultiply(vector);
+        RealVector h = new ArrayRealVector(h1);
+        h.mapMultiplyToSelf(k);
+        for (int idx = 1; idx <= k; idx++) {
+            RealVector tempH2 = new ArrayRealVector(h2);
+            h = h.add(tempH2.mapMultiplyToSelf(idx));
+        }
+        h.mapToSelf(new Mod(1000));
+
+        ByteBuffer bb = ByteBuffer.allocate(this.l * 8);
+        DoubleBuffer buffer = bb.asDoubleBuffer();
+        buffer.put(h.toArray());
+        return BitSet.valueOf(bb.array());
     }
 
     @Override
     public BitSet hash(ArrayRealVector vector) {
-        return null;
+        RealVector h1 = this.hash1.preMultiply(vector);
+        RealVector h2 = this.hash2.preMultiply(vector);
+        h1.mapMultiplyToSelf(k);
+        for (int idx = 1; idx <= k; idx++) {
+            h1 = h1.add(h2.mapMultiplyToSelf(idx));
+        }
+        h1.mapToSelf(new Mod(1000));
+
+        ByteBuffer bb = ByteBuffer.allocate(this.l * 8);
+        DoubleBuffer buffer = bb.asDoubleBuffer();
+        buffer.put(h1.toArray());
+        return BitSet.valueOf(bb.array());
     }
 
     @Override
@@ -102,10 +124,16 @@ public class EuclideanHash implements VHash {
         private static final EuclideanHash instance = EuclideanHash.getNewInstance();
     }
 
-    private static class ModPrime implements UnivariateFunction {
+    private static class Mod implements UnivariateFunction {
+        private int modulo;
+
+        public Mod(int modulo) {
+            this.modulo = modulo;
+        }
+
         @Override
         public double value(double x) {
-            return x % EuclideanHash.primeNumber;
+            return x % this.modulo;
         }
     }
 }
